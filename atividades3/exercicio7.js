9a081cd457a4fb1957c8d2a16f9f9eda372d6717
Gabrieli Martins <gabrielidelimamartins@gmail.com>
2025-11-03 23:28:54 -0300

    Update exercicio7.js

--- atividades3/exercicio7.js
+++ atividades3/exercicio7.js
@@ -2,11 +2,17 @@ import readline from 'readline-sync';
 
 const vetorNumero = [2, 5, 1, 3, 4, 9, 7, 8, 10, 6];
 let encontrarNumero = readline.questionInt("Digite o numero que voce deseja encontrar: ");
+let numeroEncontrado = false;
 
-const i = vetorNumero.indexOf(encontrarNumero);
+for (let i = 0; i < vetorNumero.length; i++) {
+    if (vetorNumero[i] === encontrarNumero) {
+        console.log(`O número ${encontrarNumero} está localizado na posição: ${i}`);
+        numeroEncontrado = true;
+        break;
+    }
+}
 
-if(i !== -1){
-    console.log(`O número ${encontrarNumero} está localizado na posição: ${i}`);
-} else {
+if (!numeroEncontrado) {
     console.log(`O número ${encontrarNumero} não foi encontrado!`);
 }
+
